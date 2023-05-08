deef4893fa607018ac7adede32488fb7a1fa36c9
FrostyFreet <93837203+FrostyFreet@users.noreply.github.com>
2023-05-08 19:16:12 +0200

    Add files via upload

---
+++ Stop-watch/stop.js
@@ -0,0 +1,83 @@
+let seconds=0;
+let displaySeconds=0;
+let minutes=0;
+let displayMinutes=0;
+let hours=0;
+let displayHours=0;
+let second=15000;
+let intervalId = null;
+
+function loop() {
+  intervalId = setInterval(() => {
+    seconds++;
+    if (seconds < 10) {
+      $("#timer").text("0" + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds);
+    }
+
+    if (seconds === 10) {
+      displaySeconds += 1;
+      seconds = 0;
+      $("#timer").text("" + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds)
+    }
+
+    if (displaySeconds === 6) {
+      minutes++;
+      displaySeconds = 0;
+      seconds = 0;
+      $("#timer").text("" + displayHours + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds);
+    }
+
+    if (minutes === 10) {
+      displaySeconds = 0;
+      seconds = 0;
+      minutes = 0;
+      displayMinutes += 1;
+      $("#timer").text("" + displayHours + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds);
+    }
+
+    if (displayMinutes === 6) {
+      displaySeconds = 0;
+      seconds = 0;
+      minutes = 0;
+      displayMinutes = 0;
+      hours += 1;
+      $("#timer").text("" + displayHours + hours + ":" + displayMinutes + minutes + ":" + displaySeconds + seconds);
+    }
+
+  }, 1000);
+}
+
+    
+
+
+
+
+$(".start").click(function () { 
+    loop();
+
+});
+
+$(".reset").click(function () {    
+    clearInterval(intervalId);
+    seconds=0;
+    displaySeconds=0;
+    minutes=0;
+    displayMinutes=0;
+    hours=0;
+    displayHours=0;
+    $("#timer").text(""+displayHours+hours+":"+displayMinutes+minutes+":"+displaySeconds+seconds);
+
+}
+)
+    
+$(".stop").click(function () { 
+    
+    clearInterval(intervalId);
+
+}) 
+
+
+
+
+ 
+
